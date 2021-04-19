4748218d4beca25232685a81fb958e0297ed0030
poojabendale16 <poojabendale2012@gmail.com>
2021-04-19 14:09:50 +0530

    Initial commit

---
+++ Chap 1/hello.py
@@ -0,0 +1,8 @@
+# Author:Pooja
+'''
+1
+2
+3
+'''
+import os
+print("Hello world")
---
+++ Chap 1/play.py
@@ -0,0 +1,3 @@
+import playsound
+playsound('C:\\Users\\91970\\OneDrive\\Documents\\py\\Chap 1\\play.mp3')
+# Add double back slash as single back slash is a escape sequence and here we are going in to play the file
